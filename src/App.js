1e7cd2a3269aa9d69aa2e707aca111852b883e50
muhammadaliatk <alinoorhussainsami11@gmail.com>
2023-01-13 11:09:47 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import './App.css';
+import {Task} from './Task'
 function App() {
+  const [todolist,setTodoList] = useState([]);
+  const [newTask,setNewTask] = useState('');
+  const handleChange =(event) => {
+    
+    setNewTask(event.target.value);
+  }
+  const addTask = () =>{
+    // count = count + 1;
+    const task = {
+      id: todolist.length === 0 ? 1 : todolist[todolist.length -1].id+1,
+      taskName:newTask
+    }
+    console.log('task is ',task)
+    const newTodoList = [...todolist,task]
+    setTodoList(newTodoList)
+  }
+  const deleteTask = (task) => {
+    const newTodoList = todolist.filter((taskName) => {
+      if(task === taskName.id){
+        return false
+      }
+      else{
+        return true;
+      }
+    })
+    setTodoList(newTodoList)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+   <div className='App'>
+    <div className='addTask'>
+      <input onChange={handleChange} />
+      <button onClick={addTask}>Add Task</button>
     </div>
+    <div className='list'>
+      {todolist.map((task) => {
+       
+       return <Task taskName={task.taskName} id = {task.id} deleteTask = {deleteTask}/>
+       
+      })}
+    </div>
+   </div>
   );
 }
 
